ca5e91b4e3e9d05a6a0fc34114f714bb99215040
Celine Yoon <celine.sy.yoon@gmail.com>
2023-04-03 01:15:41 +0900

    Add : Router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-
-    if (todo === "") {
-      return;
-    }
-    setTodos((current) => [todo, ...current]);
-    setTodo("");
-  };
-
   return (
-    <div>
-      <h1>To Do List ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholder=" Write your to do..."
-        />
-        <button type="submit">Add To Do</button>
-        <hr />
-        <ul>
-          {todos.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,8 @@
+function Detail() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+export default Detail;
